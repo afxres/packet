1f95d778026ee0a179a9c6e5e0db3c1dce9c70c7
miko <stdarg@outlook.com>
2017-08-05 00:57:59 +0800

    修改示例

--- Exchange/Sample/Program.cs
+++ Exchange/Sample/Program.cs
@@ -1,11 +1,5 @@
 ﻿using Mikodev.Network;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Net;
-using System.Text;
 
 namespace Mikodev.Test
 {
@@ -13,107 +7,26 @@ namespace Mikodev.Test
     {
         static void Main(string[] args)
         {
-            var wtr = new PacketWriter()
-                .Push("len", 1F)
-                .Push("long", 32768L)
-                .Push("addr", IPAddress.Parse("192.168.1.1"))
-                .Push("endpoint", new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7500))
-                .Push("bytes", 0xFF7F4000)
-                .PushList("short", new byte[] { 0x11, 0x22 })
-                .PushList("array", new int[] { 1, 2, 3, 4 })
-                .PushList("value", new string[] { "hello", "sharp", "net" })
-                .Push("inner", new PacketWriter()
-                    .Push("one", "inner.one")
-                    .Push("two", DateTime.Now)
-                    .Push("three", 1.00M)
-                    .Push("four", new PacketWriter()
-                        .Push("one", Guid.NewGuid())
-                    )
-                );
-
-            var dwt = wtr as dynamic;
-            dwt.temp = "hello";
-            dwt.list.one = 1;
-            dwt.list.two = new string[] { "A", "Bo", "Can" };
-            dwt.list.three = new List<float>() { 1.1F, 2.2F, 3.3F };
-            dwt.buffer = new byte[4];
-
-            var buf = wtr.GetBytes();
-            var res = new PacketReader(buf);
-            var dre = res as dynamic;
-
-            Console.WriteLine(res["len"].Pull<int>().ToString("x"));    // float -> int
-            Console.WriteLine(res["long"].Pull<short>()); // long -> short
-            Console.WriteLine(res["addr"].Pull<IPAddress>());
-            Console.WriteLine(res["endpoint"].Pull<IPEndPoint>());
-            Console.WriteLine(res["bytes"].Pull<byte[]>().GetView());   // uint -> byte[] RAW Mode
-            Console.WriteLine(res["short"].Pull<ushort>().ToString("x"));   // byte[] -> ushort
-            Console.WriteLine(res["array"].PullList<int>().GetView());
-            Console.WriteLine(res["value"].PullList<string>().GetView());
-            Console.WriteLine(res.Pull("inner").Pull("one").Pull<string>());
-            Console.WriteLine(res["inner"]["two"].Pull<DateTime>());
-            Console.WriteLine(res["inner/three"].Pull<decimal>());
-            Console.WriteLine(res[@"inner\four\one"].Pull<Guid>());
-            Console.WriteLine();
-
-            Console.WriteLine((string)dre.temp);
-            Console.WriteLine((int)dre.list.one);
-            Console.WriteLine(res["array"].PullList(typeof(int)).GetView());
-            Console.WriteLine(((IEnumerable<string>)dre.list.two).GetView());
-            Console.WriteLine(((IEnumerable<float>)dre.list.three).GetView());
-            Console.WriteLine();
-
-            var obj = PacketWriter.Serialize(new
+            // serialize an anonymous object to byte array.
+            var buffer = PacketWriter.Serialize(new
             {
                 id = 1,
+                name = "mikodev",
                 data = new
                 {
-                    word = "word",
-                    endpoint = new IPEndPoint(IPAddress.Any, IPEndPoint.MinPort),
-                    none = default(string),
-                    array = new int[] { 1, 2, 3, },
-                    list = new List<byte[]>() { new byte[2], new byte[3] },
-                },
-                empty = new { },    // empty node, same as null value or empty array
-            });
+                    token = Guid.NewGuid(),
+                    last_time = DateTime.Now,
+                }
+            }).GetBytes();
 
-            var ser = obj.GetBytes();
-            var dse = new PacketReader(ser);
+            var reader = new PacketReader(buffer);
 
-            Console.WriteLine(dse["id"].Pull<int>());
-            Console.WriteLine(dse["data/word"].Pull<string>());
-            Console.WriteLine(dse["data/endpoint"].Pull<IPEndPoint>());
-            var bts = dse["data/list"].PullList<byte[]>();
-            Console.WriteLine(bts.Count());
-            Console.WriteLine();
+            Console.WriteLine($"my id : {reader["id"].Pull<int>()}");
+            Console.WriteLine($"my name : {reader["name"].Pull<string>()}");
+            Console.WriteLine($"my token : {reader["data/token"].Pull<Guid>()}");
+            Console.WriteLine($"my last online time : {reader["data/last_time"].Pull<DateTime>():yyyy-MM-dd HH:mm:ss}");
 
-            foreach (var k in dse["data"].Keys)
-                Console.WriteLine(k);
-
-            var exp = new ExpandoObject() as dynamic;
-            exp.a = 1;
-            exp.b = "two";
-            exp.c = new Dictionary<string, object>()
-            {
-                ["a"] = 1,
-                ["b"] = "two",
-            };
-            var ans = PacketWriter.Serialize(exp);
-        }
-
-    }
-
-    static class Modules
-    {
-        public static string GetView(this IEnumerable lst)
-        {
-            var stb = new StringBuilder("[");
-            var spl = ", ";
-            foreach (var i in lst)
-                stb.Append(i).Append(spl);
-            stb.Length -= spl.Length;
-            stb.Append("]");
-            return stb.ToString();
+            // more samples, see unit test.
         }
     }
 }
