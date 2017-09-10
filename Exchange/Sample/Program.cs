19d56f95686dcea614dad9752c140f691850bf0f
miko <stdarg@outlook.com>
2017-09-10 17:24:25 +0800

    修改示例

--- Exchange/Sample/Program.cs
+++ Exchange/Sample/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Mikodev.Network;
 using System;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -23,7 +22,7 @@ namespace Mikodev.Test
                 Console.WriteLine($"id : {reader["id"].Pull<int>()}");
                 Console.WriteLine($"name : {reader["name"].Pull<string>()}");
                 Console.WriteLine($"token : {reader["data/token"].Pull<Guid>()}");
-                Console.WriteLine($"timestamp : {reader["data/timestamp"].Pull<DateTime>():yyyy-MM-dd HH:mm:ss}");
+                Console.WriteLine($"timestamp : {reader["data/timestamp"].Pull<DateTime>():u}");
                 Console.WriteLine();
             }, null);
 
@@ -44,49 +43,6 @@ namespace Mikodev.Test
             Thread.Sleep(1000);
 
             // more samples, see unit test.
-
-            var times = 1 << 20;
-
-            for (int i = 0; i < 1 << 26; i++)
-            {
-                var t = i * i;
-            }
-
-            // 12 ~ 13 ms Debug
-            using (var t = new TraceWatch())
-            {
-                for (var i = 0; i < times; i++)
-                {
-                    var buf = BitConverter.GetBytes(i);
-                    var res = BitConverter.ToInt32(buf, 0);
-                }
-            }
-
-            // 230 ~ 240 ms Debug
-            using (var t = new TraceWatch())
-            {
-                for (var i = 0; i < times; i++)
-                {
-                    var buf = PacketWriter.Serialize(i).GetBytes();
-                    var res = new PacketReader(buf).Pull<int>();
-                }
-            }
-        }
-    }
-
-    class TraceWatch : IDisposable
-    {
-        private Stopwatch _watch = new Stopwatch();
-
-        public TraceWatch()
-        {
-            _watch.Start();
-        }
-
-        public void Dispose()
-        {
-            _watch.Stop();
-            Console.WriteLine($"[{_watch.ElapsedMilliseconds} ms]");
         }
     }
 }
